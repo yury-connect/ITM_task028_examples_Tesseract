f341b2481c5c83b8a9bf456901bad224447c6f69
Yury Lapitski <yury.connect@gmail.com>
2025-06-27 17:05:58 +0300

    работает!

--- src/main/java/dev/it_mentor/demo/model/RecognizedText.java
+++ src/main/java/dev/it_mentor/demo/model/RecognizedText.java
@@ -5,29 +5,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 @Entity
-@NoArgsConstructor
-@EqualsAndHashCode(exclude = {"id", "recognitionDate"})
-@ToString
-@Getter
+//@NoArgsConstructor
+//@EqualsAndHashCode(exclude = "id")
+//@ToString
+//@Getter
+@Data
+@Table(name = "texttes")
 public class RecognizedText {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
-    @Column(name = "rt_text")
+    @Column(name = "rt_info", columnDefinition = "TEXT")
+    private String fileInfo;
+
+    @Column(name = "rt_text", columnDefinition = "TEXT")
     private String text;
 
     @Column(name = "rt_date")
+    @CreationTimestamp
     private LocalDateTime recognitionDate;
 }
 
